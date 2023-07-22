f5359f75555a500502e4c7d43b61088b3348c74e
Tariq-Mustafa <tariqmustafaabdullah@gmail.com>
2023-07-22 09:10:52 -0700

    preliminary example for main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef _PRINTF_H
+#define _PRINTF_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+/* _prinf.c */
+int _printf(const char *format, ...);
+
+#endif
