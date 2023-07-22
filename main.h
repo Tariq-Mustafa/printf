20be274b92bdc9e439a862a0fc78347315d8e9c1
Tariq-Mustafa <tariqmustafaabdullah@gmail.com>
2023-07-22 07:41:23 -0700

    test

--- main.h
+++ main.h
@@ -1,11 +1,77 @@
-main.h
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _PRINTF_H
+#define _PRINTF_H
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
 
-#define BUFF_SIZE 1024
+#define OUTPUT_BUF_SIZE 1024
+#define BUF_FLUSH -1
 
+
+#define NULL_STRING "(null)"
+
+#define PARAMS_INIT {0, 0}
+
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
+/**
+ * struct parameters - parameters struct
+ *
+ * @h_modifier: on if h_modifier is specified
+ * @l_modifier: on if l_modifier is specified
+ *
+ */
+typedef struct parameters
+{
+	unsigned int h_modifier		: 1;
+	unsigned int l_modifier		: 1;
+} params_t;
+
+/**
+ * struct specifier - Struct token
+ *
+ * @specifier: format token
+ * @f: The function associated
+ */
+typedef struct specifier
+{
+	char *specifier;
+	int (*f)(va_list, params_t *);
+} specifier_t;
+
+/* _put.c module */
+int _puts(char *str);
+int _putchar(int c);
+
+/* _prinf.c module */
+int _printf(const char *format, ...);
+
+/* print_functions.c */
+int print_char(va_list ap, params_t *params);
+int print_int(va_list ap, params_t *params);
+int print_string(va_list ap, params_t *params);
+int print_percent(va_list ap, params_t *params);
+
+/* specifier.c module */
+int (*get_specifier(char *s))(va_list ap, params_t *params);
+int get_print_func(char *s, va_list ap, params_t *params);
+int get_modifier(char *s, params_t *params);
+
+/* simple_print.c */
+int print_from_to(char *start, char *stop, char *except);
+
+/* params.c module */
+void init_params(params_t *params, va_list ap);
+
+/* number.c module */
+int print_unsigned(va_list ap, params_t *params);
+
+/* print_number.c module */
+int _isdigit(int c);
+int _strlen(char *s);
 
 #endif
---
+++ specifier.c
@@ -0,0 +1,70 @@
+#include "main.h"
+
+/**
+ * get_specifier - finds the format func
+ * @s: the format string
+ *
+ * Return: the number of bytes printed
+ */
+int (*get_specifier(char *s))(va_list ap, params_t *params)
+{
+	specifier_t specifiers[] = {
+		{"c", print_char},
+		{"d", print_int},
+		{"i", print_int},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (specifiers[i].specifier)
+	{
+		if (*s == specifiers[i].specifier[0])
+		{
+			return (specifiers[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * get_print_func - finds the format func
+ * @s: the format string
+ * @ap: argument pointer
+ * @params: the parameters struct
+ *
+ * Return: the number of bytes printed
+ */
+int get_print_func(char *s, va_list ap, params_t *params)
+{
+	int (*f)(va_list, params_t *) = get_specifier(s);
+
+	if (f)
+		return (f(ap, params));
+	return (0);
+}
+
+/**
+ * get_modifier - finds the modifier func
+ * @s: the format string
+ * @params: the parameters struct
+ *
+ * Return: if modifier was valid
+ */
+int get_modifier(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*s)
+	{
+	case 'h':
+		i = params->h_modifier = 1;
+		break;
+	case 'l':
+		i = params->l_modifier = 1;
+		break;
+	}
+	return (i);
+}
